ce0493a046a160b91b02015224f07955b11a2fa7
Noah Moats <85518918+noahmoats@users.noreply.github.com>
2021-07-28 23:52:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,13 @@
 $(document).ready(function(){
   
 });
+$(function(){
+  $('#console-box').append('console is ready...');
+  
+  $('li').click(function(){
+    $('#console-box').append('<br>class:'+$(this).attr("class")+'|value:'+$(this).text()+'.');
+    $(this).toggleClass('selected').siblings().removeClass('selected');
+; 
+  });
+  
+});
